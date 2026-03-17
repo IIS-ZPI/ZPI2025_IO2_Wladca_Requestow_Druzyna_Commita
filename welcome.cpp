716bb5c0a229b1242933709307fee20e0f6609ec
Tymoteusz Kot <252803@edu.p.lodz.pl>
2026-03-17 20:47:44 +0100

    LAB1 - ZAD 2

---
+++ welcome.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::string msg =   "Group name : Wladca_Requestow_Druzyna_Commita\n"
+                        "SCRUM Master : Tymoteusz Kot\n"
+                        "Github : 252803@edu.p.lodz.pl";
+
+    std::cout << msg << std::endl;
+    
+    return 0;
+}
