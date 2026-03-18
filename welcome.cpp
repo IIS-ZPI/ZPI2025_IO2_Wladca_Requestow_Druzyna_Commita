dc906742283a8301f72baa2e69cd82104656aafb
Szymon Pokora <251205@edu.p.lodz.pl>
2026-03-18 20:01:01 +0100

    Lab1_Zad3

--- welcome.cpp
+++ welcome.cpp
@@ -3,9 +3,11 @@
 int main() {
     std::string msg =   "Group name : Wladca_Requestow_Druzyna_Commita\n"
                         "SCRUM Master : Tymoteusz Kot\n"
-                        "Github : 252803@edu.p.lodz.pl"
+                        "Github : 252803@edu.p.lodz.pl\n"
                         "Tester : Tobiasz Grala\n"
-                        "Github : Pjongi";
+                        "Github : Pjongi\n"
+                        "Developer : Szymon Pokora\n"
+                        "Github : SzymonPokora\n";
 
     std::cout << msg << std::endl;
     
